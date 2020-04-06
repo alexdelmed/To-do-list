9fec7b63b0c21f1eb6f33e6cd736349217a3630d
alexdelmed <alexdm14@live.com.mx>
2020-04-06 16:25:05 -0500

    Add files via upload

---
+++ app.module.ts
@@ -0,0 +1,21 @@
+//app bootstraping file
+
+  import { BrowserModule } from '@angular/platform-browser';
+  import { NgModule } from '@angular/core';
+  import { FormsModule } from '@angular/forms';
+  import { HttpModule } from '@angular/http';
+
+  import {TodosComponent} from './components/todos/todos';
+
+  @NgModule({
+    declarations: [ TodosComponent ],
+    imports: [
+      BrowserModule,
+      FormsModule,
+      HttpModule
+    ],
+    providers: [],
+    bootstrap:    [ TodosComponent ]
+  })
+  export class AppModule { }
+  
